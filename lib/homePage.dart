36affc4e3ea8d2de611285e3c6cbd3b2a2fab8c0
Raajeev Chandran <raajeevchandran787@gmail.com>
2021-04-24 20:34:22 +0530

    ui changes

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:skinmed/detectDisease.dart';
+import 'detectDisease.dart';
 
 class HomeScreen extends StatelessWidget {
   final List<dynamic> trivia = [
