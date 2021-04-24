94c28ae382879a270681ed31f294c4a0bc170cb6
Raajeev Chandran <raajeevchandran787@gmail.com>
2021-04-24 13:43:00 +0530

    homescreen

---
+++ lib/homePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Color(0xFFFf2f6fe),
+          body: ,
+    ));
+  }
+}
